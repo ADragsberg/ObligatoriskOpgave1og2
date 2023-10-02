780fa2c468d3a89955edc7f2504373948cab82b4
Andreas Dragsberg <115148828+ADragsberg@users.noreply.github.com>
2023-10-02 15:30:59 +0200

    OPG 1 og 2 Done

--- BookLib/BooksRepository.cs
+++ BookLib/BooksRepository.cs
@@ -8,17 +8,32 @@ namespace BookLib
 {
     public class BooksRepository : IBooksRepository
     {
-        private List<Book> _books = new List<Book>();
+        private int _nextId = 1;
+        private List<Book> _books = new List<Book>()
+        {
+           new Book("Bog1", 500) { Id = 1 },
+           new Book("Bog2", 500) { Id = 2 },
+           new Book("Bog3", 500) { Id = 3 },
+           new Book("Bog4", 500) { Id = 4 },
+           new Book("Bog5", 500) { Id = 5 }
+        };
         public Book Add(Book book)
         {
             book.Validate();
+            book.Id = _nextId++;
             _books.Add(book);
             return book;
         }
 
-        public Book Delete(int id)
+        public Book? Delete(int id)
         {
-            throw new NotImplementedException();
+            Book? bookToRemove = this.GetById(id);
+            if (bookToRemove != null)
+            {
+                _books.Remove(bookToRemove);
+            }
+
+            return bookToRemove;
         }
 
         public List<Book> Get()
@@ -27,14 +42,29 @@ namespace BookLib
             return result;
         }
 
-        public Book GetById(int id)
+        public Book? GetById(int id)
         {
-            throw new NotImplementedException();
+            Book? result = _books.FirstOrDefault(t => t.Id == id);
+
+            return result;
         }
 
         public Book Update(int id, Book book)
         {
-            throw new NotImplementedException();
+            book.Validate();
+            Book? bookToUpdate = this.GetById(id);
+            if (bookToUpdate != null) 
+            {
+                bookToUpdate.Title = book.Title;
+                bookToUpdate.Price = book.Price;
+            }
+
+            return bookToUpdate;
+
+        }
+        private int NextId()
+        {
+            return _nextId++;
         }
     }
 }
--- BookLibTests/BooksRepositoryTests.cs
+++ BookLibTests/BooksRepositoryTests.cs
@@ -18,48 +18,64 @@ namespace BookLib.Tests
         private Book _defaultPriceBook = new Book() { Id = 5, Title = "default Price" };
         private Book _negativePriceBook = new Book("NegativePrice", -1) { Id = 6 };
         private Book _zeroPriceBook = new Book("ZeroPrice", 0) { Id = 7 };
-        private List<Book> _testList = new List<Book>() { };
-        private BooksRepository _booksRepo = new BooksRepository();
-        
+        private Book _validBook2 = new Book("ValidBog2", 505) { Id = 8 };
+
+        private IBooksRepository _booksRepo = new BooksRepository();
+        private IBooksRepository _booksRepoFixed = new BooksRepository();
+
         [TestMethod()]
         public void AddTest()
         {
-            Assert.AreEqual<int>(0, _booksRepo.Get().Count());
+            // Test om de tilføjes og får unik id.
+            Assert.AreEqual<int>(5, _booksRepo.Get().Count());
+            Book abcd = _booksRepo.Add(new Book { Title = "abcd", Price = 600 });
+            Book efgh = _booksRepo.Add(new Book { Title = "efgh", Price = 700 });
+            Assert.IsNotNull(abcd.Id);
+            Assert.IsNotNull(efgh.Id);
+            Assert.AreNotEqual(abcd.Id, efgh.Id);
+            Assert.IsTrue(abcd.Id < efgh.Id);
+            Assert.AreEqual<int>(7, _booksRepo.Get().Count());
 
-            _booksRepo.Add(_validBook);
 
-            Assert.AreEqual<int>(1, _booksRepo.Get().Count());
-
+            //test validering ikke tillader tilføjelse af fejlede objekter
             Assert.ThrowsException<ArgumentNullException>(() => _booksRepo.Add(_nullTitleBook));
             Assert.ThrowsException<ArgumentException>(() => _booksRepo.Add(_shortTitleBook));
             Assert.ThrowsException<ArgumentException>(() => _booksRepo.Add(_priceTooHighBook));
             Assert.ThrowsException<ArgumentException>(() => _booksRepo.Add(_defaultPriceBook));
             Assert.ThrowsException<ArgumentException>(() => _booksRepo.Add(_negativePriceBook));
             Assert.ThrowsException<ArgumentException>(() => _booksRepo.Add(_zeroPriceBook));
-        }
-
-        [TestMethod()]
-        public void DeleteTest()
-        {
-            Assert.Fail();
+            Assert.AreEqual<int>(7, _booksRepo.Get().Count());
         }
 
         [TestMethod()]
         public void GetTest()
         {
-            Assert.Fail();
-        }
 
-        [TestMethod()]
-        public void GetByIdTest()
-        {
-            Assert.Fail();
+            //Test om Listen er den samme eller en kopi.
+            List<Book> savedState = new List<Book>();
+            savedState = _booksRepo.Get();
+            _booksRepo.Delete(1);
+            Assert.AreNotEqual(savedState.Count(), _booksRepo.Get().Count());
+            Assert.AreEqual(5, savedState.Count());
+            Assert.AreEqual(4, _booksRepo.Get().Count());
         }
 
         [TestMethod()]
         public void UpdateTest()
         {
-            Assert.Fail();
+            // sikrer om de to repos er enige om det gældende objekt
+            Assert.AreEqual(_booksRepoFixed.GetById(1).ToString(),_booksRepo.GetById(1).ToString());
+
+            _booksRepo.Update(1, _validBook);
+            // test om det nye objekt er ændret til det forventede
+            Assert.AreNotEqual(_booksRepoFixed.GetById(1).ToString(), _booksRepo.GetById(1).ToString());
+            Assert.AreEqual(_validBook.ToString(), _booksRepo.GetById(1).ToString());
+
+            // Exeption hvis nye book fejler validering.
+            Assert.ThrowsException<ArgumentException>(() => _booksRepo.Update(1, _negativePriceBook));
+
+            // Hvis id ikke eksisterer i repo skal der returneres null
+            Assert.IsNull(_booksRepo.Update(10, _validBook));
         }
     }
 }
